4af1063ac6e3fdef7eb2a243254a62d56ef423ca
Erina Dwi Yanti <112475552+erinady@users.noreply.github.com>
2025-04-05 12:04:47 +0700

    feat(theme): Menambahkan color sesuai color pallete bookphoria
    Menambahkan beberapa color yang belum di deklarasikan pada color theme

--- app/src/main/java/com/example/bookphoria/ui/theme/Color.kt
+++ app/src/main/java/com/example/bookphoria/ui/theme/Color.kt
@@ -10,5 +10,11 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 val DarkIndigo = Color(0xFF4A4A68)
-val SoftCream = Color(0xFFF2EFEA)
+
+// Color pallete Bookphoria
+val DeepBlue = Color(0xFF01408D)
+val MediumBlue = Color(0xFF3562B3)
+val LightBlue = Color(0xFF96ADD6)
 val PrimaryOrange = Color(0xFFE75234)
+val SoftPink = Color(0xFFF8B8AF)
+val SoftCream = Color(0xFFF2EFEA)
